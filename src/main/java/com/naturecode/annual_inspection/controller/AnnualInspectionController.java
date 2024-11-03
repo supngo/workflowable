6ceea2df093faf02418234872b1aaa874b22bb55
Thom Ngo <supngo@yahoo.com>
2024-11-03 16:12:31 -0500

    updated controller to get refactor endpoints

--- src/main/java/com/naturecode/annual_inspection/controller/AnnualInspectionController.java
+++ src/main/java/com/naturecode/annual_inspection/controller/AnnualInspectionController.java
@@ -37,53 +37,129 @@ public class AnnualInspectionController {
   }
 
   @PostMapping("/start")
-  public TaskDTO startAnnualInspectionProcess(@RequestParam String servicer) {
+  public String startAnnualInspectionProcess(@RequestParam(required = true) String servicer) {
     Map<String, Object> variables = new HashMap<>();
     variables.put("servicer", servicer); // Assign the servicer role
 
-    runtimeService.startProcessInstanceByKey("annualInspection", variables);
-    Task task = taskService.createTaskQuery().taskAssignee(servicer).singleResult();
-    TaskDTO newTask = new TaskDTO(task.getId(), task.getName(), servicer);
-    return newTask;
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("annualInspection", variables);
+    log.info("New Task: " + processInstance.getId() + " created!");
+    return "New Task: " + processInstance.getId() + " created!";
   }
 
   @GetMapping("/tasks")
   public List<TaskDTO> getTasks(@RequestParam(required = false) String assignee) {
     List<Task> tasks = taskService.createTaskQuery().list();
+    tasks.stream().forEach(task -> log.info("taskId: " + task.getId()));
     return tasks.stream()
         .map(task -> new TaskDTO(task.getId(), task.getName(), task.getAssignee()))
         .collect(Collectors.toList());
   }
 
-  @PostMapping("/complete/{taskId}")
-  public ResponseEntity<String> completeTask(
+  @PostMapping("/servicer/{taskId}")
+  public ResponseEntity<String> servicer(
         @PathVariable(required = true) String taskId, 
-        @RequestParam(required = false) String analyst, 
-        @RequestParam(required = false) String manager, 
-        @RequestParam(required = false) String decision, 
+        @RequestParam(required = true) String action,
+        @RequestParam(required = true) String analyst, 
+        @RequestParam(required = false) String comment) {
+    Map<String, Object> variables = new HashMap<>();
+    try {
+      if (action == null || (!action.equals("submit") && !action.equals("edit"))) {
+        log.info("Missing action for servicer with task: " + taskId);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An action is required and can either be submit or edit");
+      }
+      if (comment != null) {
+        variables.put("comment", comment);
+      }
+      variables.put("analyst", analyst);
+      taskService.complete(taskId, variables);
+      log.info("A servicer has done '" + action + "' for task: " + taskId);
+      return ResponseEntity.ok("A servicer has done '" + action + "' for task: " + taskId);
+    } catch (FlowableObjectNotFoundException e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("/servicer -> Task: " + taskId + " not found.");
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("/servicer -> Task: " + taskId + " got error: " + e.getMessage());
+    }
+  }
+
+  @PostMapping("/analyst/{taskId}")
+  public ResponseEntity<String> analyst(
+        @PathVariable(required = true) String taskId, 
+        @RequestParam(required = false) String servicer,
+        @RequestParam(required = false) String manager,
+        @RequestParam(required = true) String decision, 
         @RequestParam(required = false) String comment) {
     Map<String, Object> variables = new HashMap<>();
     try {
       if (comment != null) {
         variables.put("comment", comment);
       }
-      if (decision != null) {
-        variables.put("decision", decision);
+      if (decision.equals("reject") || decision.equals("send_back")) {
+        if (servicer == null || servicer.trim().length() < 1) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A servicer is required!");
+        }
+        variables.put("servicer", servicer);
       }
-      if (analyst != null) {
-        variables.put("analyst", analyst);
-      }
-      if (manager != null) {
+      else if (decision.equals("approve")) {
+        if (manager == null || manager.trim().length() < 1) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A manager is required!");
+        }
         variables.put("manager", manager);
+      } else {
+        log.info("Incorrect decision '" + decision + "' for task: " + taskId);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A decision either be approve, reject, or send_back");
       }
+      variables.put("decision", decision);
       taskService.complete(taskId, variables);
-      return ResponseEntity.ok("Task with ID " + taskId + " completed.");
+      log.info("An analyst chose to '" + decision + "' for task: " + taskId);
+      return ResponseEntity.ok("An analyst chose to '" + decision + "' for task: " + taskId);
     } catch (FlowableObjectNotFoundException e) {
       e.printStackTrace();
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task with ID " + taskId + " not found.");
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("/analyst -> Task: " + taskId + " not found.");
     } catch (Exception e) {
       e.printStackTrace();
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("/analyst -> Task: " + taskId + " got error: " + e.getMessage());
+    }
+  }
+
+  @PostMapping("/manager/{taskId}")
+  public ResponseEntity<String> manager(
+        @PathVariable(required = true) String taskId, 
+        @RequestParam(required = false) String servicer,
+        @RequestParam(required = false) String analyst,
+        @RequestParam(required = true) String decision, 
+        @RequestParam(required = false) String comment) {
+    Map<String, Object> variables = new HashMap<>();
+    try {
+      if (comment != null) {
+        variables.put("comment", comment);
+      }
+      if (decision.equals("reject")) {
+        if (servicer == null || servicer.trim().length() < 1) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A servicer is required!");
+        }
+        variables.put("servicer", servicer);
+      }
+      else if (decision.equals("send_back")) {
+        if (analyst == null || analyst.trim().length() < 1) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An analyst is required!");
+        }
+        variables.put("analyst", analyst);
+      } else if (!decision.equals("approve")) {
+        log.info("Incorrect decision '" + decision + "' for task: " + taskId);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A decision either be approve, reject, or send_back");
+      }
+      variables.put("decision", decision);
+      taskService.complete(taskId, variables);
+      log.info("A manager chose to '" + decision + "' for task: " + taskId);
+      return ResponseEntity.ok("A manager chose to '" + decision + "' for task: " + taskId);
+    } catch (FlowableObjectNotFoundException e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("/manager -> Task: " + taskId + " not found.");
+    } catch (Exception e) {
+      e.printStackTrace();
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("/manager -> Task: " + taskId + " got error: " + e.getMessage());
     }
   }
 }
